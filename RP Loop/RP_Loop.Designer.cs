2e9d0591046f13ba24eb829e14fd5bb12a15a28d
tiptop <105000222+tiptoppp@users.noreply.github.com>
2022-07-02 14:30:22 +0200

    Add files via upload

---
+++ RP Loop/RP_Loop.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace RP_Loop
+{
+    partial class RPLoopForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RPLoopForm));
+            this.ToggleRPLoopcheckBox = new System.Windows.Forms.CheckBox();
+            this.DiscordLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.MadebyLabel = new System.Windows.Forms.Label();
+            this.ToggleRPLoopLabel = new System.Windows.Forms.Label();
+            this.RPLoop = new System.Windows.Forms.GroupBox();
+            this.PublicSoloLobby = new System.Windows.Forms.GroupBox();
+            this.EmptyLobby = new System.Windows.Forms.Button();
+            this.TitleBar = new System.Windows.Forms.Panel();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Minimize = new System.Windows.Forms.Button();
+            this.searchingProcess1 = new RP_Loop.SearchingProcess();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.RPLoop.SuspendLayout();
+            this.PublicSoloLobby.SuspendLayout();
+            this.TitleBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ToggleRPLoopcheckBox
+            // 
+            this.ToggleRPLoopcheckBox.AutoSize = true;
+            this.ToggleRPLoopcheckBox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ToggleRPLoopcheckBox.ForeColor = System.Drawing.Color.White;
+            this.ToggleRPLoopcheckBox.Location = new System.Drawing.Point(118, 24);
+            this.ToggleRPLoopcheckBox.Name = "ToggleRPLoopcheckBox";
+            this.ToggleRPLoopcheckBox.Size = new System.Drawing.Size(15, 14);
+            this.ToggleRPLoopcheckBox.TabIndex = 4;
+            this.ToggleRPLoopcheckBox.UseVisualStyleBackColor = true;
+            this.ToggleRPLoopcheckBox.CheckedChanged += new System.EventHandler(this.checkBoxLoop_CheckedChanged);
+            // 
+            // DiscordLinkLabel
+            // 
+            this.DiscordLinkLabel.ActiveLinkColor = System.Drawing.Color.Magenta;
+            this.DiscordLinkLabel.AutoSize = true;
+            this.DiscordLinkLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiscordLinkLabel.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(102)))), ((int)(((byte)(231)))));
+            this.DiscordLinkLabel.Location = new System.Drawing.Point(79, 162);
+            this.DiscordLinkLabel.Name = "DiscordLinkLabel";
+            this.DiscordLinkLabel.Size = new System.Drawing.Size(76, 14);
+            this.DiscordLinkLabel.TabIndex = 7;
+            this.DiscordLinkLabel.TabStop = true;
+            this.DiscordLinkLabel.Text = "tiptop#9113";
+            this.DiscordLinkLabel.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(102)))), ((int)(((byte)(231)))));
+            this.DiscordLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.DiscordLinkLabel_LinkClicked);
+            // 
+            // MadebyLabel
+            // 
+            this.MadebyLabel.AutoSize = true;
+            this.MadebyLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MadebyLabel.ForeColor = System.Drawing.Color.White;
+            this.MadebyLabel.Location = new System.Drawing.Point(26, 162);
+            this.MadebyLabel.Name = "MadebyLabel";
+            this.MadebyLabel.Size = new System.Drawing.Size(57, 14);
+            this.MadebyLabel.TabIndex = 6;
+            this.MadebyLabel.Text = "Made by";
+            // 
+            // ToggleRPLoopLabel
+            // 
+            this.ToggleRPLoopLabel.AutoSize = true;
+            this.ToggleRPLoopLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ToggleRPLoopLabel.ForeColor = System.Drawing.Color.White;
+            this.ToggleRPLoopLabel.Location = new System.Drawing.Point(10, 24);
+            this.ToggleRPLoopLabel.Name = "ToggleRPLoopLabel";
+            this.ToggleRPLoopLabel.Size = new System.Drawing.Size(100, 14);
+            this.ToggleRPLoopLabel.TabIndex = 5;
+            this.ToggleRPLoopLabel.Text = "Toggle RP Loop";
+            // 
+            // RPLoop
+            // 
+            this.RPLoop.Controls.Add(this.ToggleRPLoopLabel);
+            this.RPLoop.Controls.Add(this.ToggleRPLoopcheckBox);
+            this.RPLoop.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RPLoop.ForeColor = System.Drawing.Color.White;
+            this.RPLoop.Location = new System.Drawing.Point(10, 32);
+            this.RPLoop.Name = "RPLoop";
+            this.RPLoop.Size = new System.Drawing.Size(145, 55);
+            this.RPLoop.TabIndex = 8;
+            this.RPLoop.TabStop = false;
+            this.RPLoop.Text = "RP Loop";
+            // 
+            // PublicSoloLobby
+            // 
+            this.PublicSoloLobby.Controls.Add(this.EmptyLobby);
+            this.PublicSoloLobby.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PublicSoloLobby.ForeColor = System.Drawing.Color.White;
+            this.PublicSoloLobby.Location = new System.Drawing.Point(10, 97);
+            this.PublicSoloLobby.Name = "PublicSoloLobby";
+            this.PublicSoloLobby.Size = new System.Drawing.Size(145, 55);
+            this.PublicSoloLobby.TabIndex = 9;
+            this.PublicSoloLobby.TabStop = false;
+            this.PublicSoloLobby.Text = "Public Solo Lobby";
+            // 
+            // EmptyLobby
+            // 
+            this.EmptyLobby.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmptyLobby.ForeColor = System.Drawing.Color.Black;
+            this.EmptyLobby.Location = new System.Drawing.Point(12, 19);
+            this.EmptyLobby.Name = "EmptyLobby";
+            this.EmptyLobby.Size = new System.Drawing.Size(121, 23);
+            this.EmptyLobby.TabIndex = 0;
+            this.EmptyLobby.Text = "Empty Lobby";
+            this.EmptyLobby.UseVisualStyleBackColor = true;
+            this.EmptyLobby.Click += new System.EventHandler(this.EmptyLobby_Click);
+            // 
+            // TitleBar
+            // 
+            this.TitleBar.Controls.Add(this.Exit);
+            this.TitleBar.Controls.Add(this.Minimize);
+            this.TitleBar.Location = new System.Drawing.Point(0, 0);
+            this.TitleBar.Name = "TitleBar";
+            this.TitleBar.Size = new System.Drawing.Size(165, 22);
+            this.TitleBar.TabIndex = 10;
+            this.TitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TitleBar_MouseDown);
+            // 
+            // Exit
+            // 
+            this.Exit.BackgroundImage = global::RP_Loop.Properties.Resources.Exit;
+            this.Exit.FlatAppearance.BorderSize = 0;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit.Location = new System.Drawing.Point(137, 0);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(28, 22);
+            this.Exit.TabIndex = 1;
+            this.Exit.TabStop = false;
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            this.Exit.MouseEnter += new System.EventHandler(this.Exit_MouseEnter);
+            this.Exit.MouseLeave += new System.EventHandler(this.Exit_MouseLeave);
+            // 
+            // Minimize
+            // 
+            this.Minimize.BackgroundImage = global::RP_Loop.Properties.Resources.Minimize;
+            this.Minimize.FlatAppearance.BorderSize = 0;
+            this.Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Minimize.Location = new System.Drawing.Point(109, 0);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(28, 22);
+            this.Minimize.TabIndex = 0;
+            this.Minimize.TabStop = false;
+            this.Minimize.UseVisualStyleBackColor = true;
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            this.Minimize.MouseEnter += new System.EventHandler(this.Minimize_MouseEnter);
+            this.Minimize.MouseLeave += new System.EventHandler(this.Minimize_MouseLeave);
+            // 
+            // searchingProcess1
+            // 
+            this.searchingProcess1.BackColor = System.Drawing.Color.Black;
+            this.searchingProcess1.Location = new System.Drawing.Point(0, 22);
+            this.searchingProcess1.Name = "searchingProcess1";
+            this.searchingProcess1.Size = new System.Drawing.Size(165, 164);
+            this.searchingProcess1.TabIndex = 11;
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // RPLoopForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(165, 186);
+            this.Controls.Add(this.searchingProcess1);
+            this.Controls.Add(this.TitleBar);
+            this.Controls.Add(this.PublicSoloLobby);
+            this.Controls.Add(this.RPLoop);
+            this.Controls.Add(this.MadebyLabel);
+            this.Controls.Add(this.DiscordLinkLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RPLoopForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "RP Loop";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.RPLoopForm_Load);
+            this.RPLoop.ResumeLayout(false);
+            this.RPLoop.PerformLayout();
+            this.PublicSoloLobby.ResumeLayout(false);
+            this.TitleBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox ToggleRPLoopcheckBox;
+        private System.Windows.Forms.LinkLabel DiscordLinkLabel;
+        private System.Windows.Forms.Label MadebyLabel;
+        private System.Windows.Forms.Label ToggleRPLoopLabel;
+        private System.Windows.Forms.GroupBox RPLoop;
+        private System.Windows.Forms.GroupBox PublicSoloLobby;
+        private System.Windows.Forms.Button EmptyLobby;
+        private System.Windows.Forms.Panel TitleBar;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Minimize;
+        private SearchingProcess searchingProcess1;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ RP Loop/RP_Loop.cs
@@ -0,0 +1,225 @@
+﻿using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using System.Runtime.InteropServices;
+using MemoryNoAdmin;
+using MProcess;
+
+namespace RP_Loop
+{
+    public partial class RPLoopForm : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+
+        public static extern bool ReleaseCapture();
+        public Mem MemLib = new Mem();
+        private bool ProcOpen = false;
+        private Process GTAV = null;
+
+        public RPLoopForm()
+        {
+            InitializeComponent();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (GTAV != null)
+                    {
+                        if (GTAV.HasExited)
+                        {
+                            GTAV = null;
+                            ProcOpen = false;
+                        }
+                    }
+                    Task task = AttemptOpenProcess();
+                }
+            });
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void RPLoopForm_Load(object sender, EventArgs e)
+        {
+            searchingProcess1.Show();
+            Timer.Enabled = true;
+            Timer.Interval = 100;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (ProcOpen == true)
+            {
+                searchingProcess1.Hide();
+            }
+        }
+
+        private async Task SoloPublicLobby()
+        {
+            int pid = ProcessAccess.GetPidWithName("GTA5");
+            ProcessAccess.SuspendProcess(pid);
+            await Task.Delay(8000);
+            ProcessAccess.ResumeProcess(pid);
+        }
+
+        private void EmptyLobby_Click(object sender, EventArgs e)
+        {
+            Task task = SoloPublicLobby();
+        }
+
+        CancellationTokenSource _cts = null;
+        private bool Connect = false;
+
+        private async Task AttemptOpenProcess()
+        {
+            if (!ProcOpen && !Connect)
+            {
+                Connect = true;
+                if (MemLib.OpenProcess("GTA5"))
+                {
+                    ProcOpen = true;
+                    await ConnectWorld();
+                    GTAV = MemLib.mProc.Process;
+                }
+                Connect = false;
+            }
+        }
+
+        private string WorldAssemblyAddress = "";
+        private string WorldBaseAddress = "";
+
+        private string WorldPlayersBaseAddress => MemLib.ReadLong(WorldBaseAddress).ToString("X2");
+
+        private string LocalPlayerAddress => MemLib.ReadLong(WorldPlayersBaseAddress + "+8").ToString("X2");
+
+        private string LocalPlayerInfoAddress => MemLib.ReadLong(LocalPlayerAddress + "+10C8").ToString("X2");
+
+        private string WantedOffset => (0x7D0 + 0xB8).ToString("X2");
+
+        public int WantedLevel
+        {
+            get => MemLib.ReadInt(LocalPlayerInfoAddress + "+" + WantedOffset);
+            set => MemLib.WriteMemory(LocalPlayerInfoAddress + "+" + WantedOffset, "int", value.ToString());
+        }
+        private string GlobalAssemblyAddress = "";
+        private string GlobalBaseAddress = "";
+
+        private string GlobalRP_Address => (MemLib.ReadLong(GlobalBaseAddress + "+8") + 0x10).ToString("X2");
+
+        public float GlobalRPValue
+        {
+            get => MemLib.ReadFloat(GlobalRP_Address);
+            set => MemLib.WriteMemory(GlobalRP_Address, "float", value.ToString());
+        }
+        private bool IsWorldConnectionAttempting = false;
+
+        private async Task ConnectWorld()
+        {
+            if (!IsWorldConnectionAttempting)
+            {
+                IsWorldConnectionAttempting = true;
+                var WorldHook = await MemLib.AoBScan(
+                MemLib.mProc.MainModule.BaseAddress.ToInt64(),
+                MemLib.mProc.MainModule.BaseAddress.ToInt64() + MemLib.mProc.MainModule.ModuleMemorySize,
+                "48 8B 05 ?? ?? ?? ?? 45 ?? ?? ?? ?? 48 8B 48 08 48 85 C9 74 07", true, true, true, "");
+                long Address = WorldHook[0];
+                WorldAssemblyAddress = (Address.ToString("X2"));
+                WorldBaseAddress = (Address + MemLib.ReadInt((Address + 3).ToString("X2")) + 7).ToString("X2");
+                var GlobalHook = await MemLib.AoBScan(
+                MemLib.mProc.MainModule.BaseAddress.ToInt64(),
+                MemLib.mProc.MainModule.BaseAddress.ToInt64() + MemLib.mProc.MainModule.ModuleMemorySize,
+                "48 8D 3D ?? ?? ?? 0? 80 3E", true, true, true, "");
+                Address = GlobalHook[0];
+                GlobalAssemblyAddress = (Address.ToString("X2"));
+                GlobalBaseAddress = (Address + MemLib.ReadInt((Address + 3).ToString("X2")) + 7).ToString("X2");
+            }
+        }
+
+        private async void checkBoxLoop_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ToggleRPLoopcheckBox.Checked)
+            {
+                GlobalRPValue = 500;
+                _cts = new CancellationTokenSource();
+                while (ToggleRPLoopcheckBox.Checked)
+                {
+                    try
+                    {
+                        await onTick(_cts.Token);
+                        await Task.Delay(1000, _cts.Token);
+                    }
+                    catch (TaskCanceledException)
+                    {
+                        break;
+                    }
+                }
+                ResetDefaults();
+            }
+            else
+            {
+                _cts?.Cancel();
+            }
+        }
+
+        private void ResetDefaults()
+        {
+            GlobalRPValue = 1;
+            WantedLevel = 0;
+        }
+
+        private async Task onTick(CancellationToken token)
+        {
+            WantedLevel = 1;
+            await Task.Delay(1000, token);
+            WantedLevel = 0;
+        }
+
+        private void Minimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void Minimize_MouseEnter(object sender, EventArgs e)
+        {
+            Minimize.BackgroundImage = Properties.Resources.Minimize_Hover;
+        }
+
+        private void Minimize_MouseLeave(object sender, EventArgs e)
+        {
+            Minimize.BackgroundImage = Properties.Resources.Minimize;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Exit_MouseEnter(object sender, EventArgs e)
+        {
+            Exit.BackgroundImage = Properties.Resources.Exit_Hover;
+        }
+
+        private void Exit_MouseLeave(object sender, EventArgs e)
+        {
+            Exit.BackgroundImage = Properties.Resources.Exit;
+        }
+
+        private void DiscordLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://discordapp.com/users/558009992473739289");
+        }
+    }
+}
